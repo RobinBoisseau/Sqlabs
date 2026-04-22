124dbdef1652b80c10b3ab04c292772b5f9bd847
Guénolé <guenole1308@gmail.com>
2026-04-22 15:39:49 +0200

    Fusion terminée : intégration de mon code avec celui de robin

--- backend/app/Http/Resources/ExerciceResource.php
+++ backend/app/Http/Resources/ExerciceResource.php
@@ -5,19 +5,21 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+
 class ExerciceResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-   public function toArray(Request $request)
+   /**
+    * Transform the resource into an array.
+    *
+    * @return array<string, mixed>
+    */
+  public function toArray(Request $request)
 {
-    return [
-        'enonce' => $this->enonce,
-        'titre' => $this->titre,
-        'type' => $this->type
-    ];
+   return [
+       'titre' => $this->titre,
+       'type' => $this->type,
+       'enonce' => $this->enonce
+       
+   ];
 }
 }
